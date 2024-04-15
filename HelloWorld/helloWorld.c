90ce94f1b425ac095a19f0817712a2a1d1b1e547
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-04-15 10:56:21 -0600

    Add files via upload

---
+++ HelloWorld/helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // Include standard input and output library
+
+int main() {        // Main function where the execution of program begins
+	char message[] = "Hello, World!";  // Declare a character array and initialize it with the message
+	printf("%s\n", message);  // Print the message stored in the variable
+	return 0;       // Return 0 to the operating system
+}
---
+++ HelloWorld/helloWorld.s
@@ -0,0 +1,22 @@
+.globl main	
+
+main:
+pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movabsq	$6278066737626506568, %rax
+	movq	%rax, -22(%rbp)
+	movabsq	$9399091170596640, %rax
+	movq	%rax, -16(%rbp)
+	leaq	-22(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	leave
+	ret
+
